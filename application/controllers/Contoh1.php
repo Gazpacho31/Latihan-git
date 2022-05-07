aa421ca6e976b048a8892afb03a4252326a02b87
Gazpacho31 <wirasn31@gmail.com>
2022-05-07 09:21:37 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,24 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo"<h1>Biodata</h1>";
+	echo"<p>Nama Lengkap</p>
+		 <code>Wira Satya Nugraha</code>
+
+		 <p>Umur</p>
+		 <code>22</code>
+
+		 <p>Jenis Kelamin</p>
+		 <code>Laki-laki</code>
+
+		 <p>Fakultas</p>
+		 <code>Teknik & Informasi</code>
+
+		 <p>Jurusan</p>
+		 <code>Teknologi Informasi</code>";
+
+		   
+    }
+}
